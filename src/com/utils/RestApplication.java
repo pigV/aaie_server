d4f09c564100047fa397d1949a5c2551cee6be7e
mengfw <mfwdhee@163.com>
2017-09-02 16:40:27 +0800

    aaie_server for jersye2 + spring4 + mybaitis

--- src/com/utils/RestApplication.java
+++ src/com/utils/RestApplication.java
@@ -1,8 +1,8 @@
 package com.utils;
 
-import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 
 /**
  * Created by Administrator on 2017/8/28 0028.
@@ -15,6 +15,6 @@ public class RestApplication extends ResourceConfig {
 //        register(JacksonJsonProvider.class);
         register(JacksonFeature.class);
         //register(JspMvcFeature.class); //注册MVC支持
-        //property(JspMvcFeature.TEMPLATE_BASE_PATH, "/");//jsp文件所在位置，当前JSP文件是在项目根目录下
+        property(JspMvcFeature.TEMPLATES_BASE_PATH, "/");//jsp文件所在位置，当前JSP文件是在项目根目录下
     }
 }
